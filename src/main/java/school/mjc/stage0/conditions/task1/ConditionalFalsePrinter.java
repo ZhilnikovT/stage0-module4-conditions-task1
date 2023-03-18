1e5ea8773682106072537d4768a99f95877c43d6
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-18 22:44:54 +0300

    Update ConditionalFalsePrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+       boolean b = false; 
+        if(false){
+        }else{
+            System.out.println(b);
+        }
+        
     }
 }
